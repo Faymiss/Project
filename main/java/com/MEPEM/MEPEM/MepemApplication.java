311d56129cd236f5546e026a2bf96f4086313c23
Faymiss <faiza0khamis@gmail.com>
2024-07-19 08:47:02 +0300

    first commit

---
+++ main/java/com/MEPEM/MEPEM/MepemApplication.java
@@ -0,0 +1,13 @@
+package com.MEPEM.MEPEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MepemApplication.class, args);
+	}
+
+}
